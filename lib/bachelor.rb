bad178f0ca0f6f6b90b69def62d51954a6b954f3
Yelstin Fernandes <yfernan2@u.rochester.edu>
2017-05-09 18:58:39 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,70 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_number, arr|
+    if season_number == season
+      arr.each_with_index do |el, idx|
+        el.each do |k, v|
+          if v == "Winner"
+            return data[season_number][idx]["name"].split.first
+          end
+        end
+      end
+    end
+  end
+  nil
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_number, arr|
+    arr.each_with_index do |el, idx|
+      el.each do |k, v|
+        if v == occupation
+          return data[season_number][idx]["name"]
+        end
+      end
+    end
+  end
+  nil
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season_number, arr|
+    arr.each_with_index do |el, idx|
+      el.each do |k, v|
+        if v == hometown
+          count += 1
+        end
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_number, arr|
+    arr.each_with_index do |el, idx|
+      el.each do |k, v|
+        if v == hometown
+          return data[season_number][idx]["occupation"]
+        end
+      end
+    end
+  end
+  nil
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  data.each do |season_number, arr|
+    if season_number == season
+      arr.each_with_index do |el, idx|
+        el.each do |k, v|
+          if k == "age"
+            ages << data[season_number][idx][k].to_i
+          end
+        end
+      end
+    end
+  end
+  (ages.reduce(:+).to_f/ages.length).round
 end
